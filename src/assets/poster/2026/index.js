0572f9ce953890f2d5ee4b4c477187de753d442c
Nahyan Sharvin <nahyansharvin@gmail.com>
2026-01-21 19:44:36 +0530

    feat: add latest poster template for stage and off stage programs

---
+++ src/assets/poster/2026/index.js
@@ -0,0 +1,11 @@
+import stage_bg from "./stage_bg.svg";
+import off_stage_bg from "./off_stage_bg.svg";
+import position_bg_stage from "./position_bg_stage.svg";
+import position_bg_off_stage from "./position_bg_off_stage.svg";
+
+export {
+    stage_bg,
+    off_stage_bg,
+    position_bg_stage,
+    position_bg_off_stage
+}
