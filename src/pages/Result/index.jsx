fc507055fb4c348954ce3513c658964e39e798f6
mohammedafeef <mohammedafeef.maf@gmail.com>
2026-01-21 21:11:48 +0530

    [style]: update result download for events

--- src/pages/Result/index.jsx
+++ src/pages/Result/index.jsx
@@ -1,21 +1,28 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Header from '@/components/ui/Header';
-import { SearchIcon } from '@/assets/icons';
-import Empty from '@/assets/gifs/empty.gif';
-import Loading from '@/assets/gifs/loading_hand.gif';
+import React, { useState, useEffect } from "react";
+import Header from "@/components/ui/Header";
+import { SearchIcon } from "@/assets/icons";
+import { Share2, Download, Loader } from "lucide-react";
+import html2canvas from "html2canvas";
+import ReactGA from "react-ga4";
+import Poster from "@/components/Poster";
+import Empty from "@/assets/gifs/empty.gif";
+import Loading from "@/assets/gifs/loading_hand.gif";
 import { motion } from "motion/react";
-import { useAutoAnimate } from '@formkit/auto-animate/react';
-
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 function Index() {
-  const navigate = useNavigate();
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedProgram, setSelectedProgram] = useState(null);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const ApiUrl = import.meta.env.VITE_API_URL;
   const [programs, setPrograms] = useState([]);
   const [filteredPrograms, setFilteredPrograms] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [parent] = useAutoAnimate()
+  // const [loadingPoster, setLoadingPoster] = useState(true);
+  const [parent] = useAutoAnimate();
+  const [imageUrl, setImageUrl] = useState("");
+  const [posterLoading, setPosterLoading] = useState(false);
+  const [showPoster, setShowPoster] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,31 +30,128 @@ function Index() {
       try {
         const response = await fetch(`${ApiUrl}/events/resultPublished`);
         const data = await response.json();
-        const sortData = data?.data.sort((a, b) => new Date(b.last_updated) - new Date(a.last_updated));
+        // console.log(data);
+        const sortData = data?.data.sort(
+          (a, b) => new Date(b.last_updated) - new Date(a.last_updated),
+        );
         setPrograms(sortData);
-        setFilteredPrograms(sortData);
+        // console.log(sortData);
+        handleProgramSelect(sortData[0]);
       } catch (error) {
         console.error(error);
       } finally {
         setLoading(false);
       }
-    }
+    };
     fetchData();
   }, []);
 
   useEffect(() => {
-    if (searchTerm !== '') {
-      const filteredProgram = programs.filter((program) => program.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    if (searchTerm !== "") {
+      setShowPoster(false);
+      const filteredProgram = programs.filter((program) =>
+        program.name.toLowerCase().includes(searchTerm.toLowerCase()),
+      );
       setFilteredPrograms(filteredProgram);
-    } else {
+    } else if (searchTerm === "") {
       setFilteredPrograms(programs);
     }
   }, [searchTerm, programs]);
 
-  const handleProgramSelect = (program) => {
-    navigate(`/result/${program._id}`);
-  };
+  useEffect(() => {
+    if (selectedProgram) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [selectedProgram]);
 
+  const handleProgramSelect = async (program) => {
+    // if (program._id === selectedProgram?.id) {
+    //   alert('Already selected');
+    //   return;
+    // }
+
+    setSelectedProgram(program);
+    setPosterLoading(true);
+
+    try {
+      const response = await fetch(`${ApiUrl}/result/event/${program._id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch program data");
+      }
+
+      const { data } = await response.json();
+      // console.log("Fetched Data:", data);
+
+      const formattedData = data.map((program) => ({
+        programName: program.name,
+        id: program._id,
+        result_no: program.serial_number,
+        stageStatus: program.is_onstage,
+        is_group: program.is_group,
+        winners: program.winningRegistrations
+          .reduce((acc, winner) => {
+            if (program.is_group && winner.eventRegistration.collegeName) {
+              // Group Winners
+              const positionIndex = acc.findIndex(
+                (w) => w.position === winner.position,
+              );
+              const newUser = {
+                name: winner.eventRegistration.collegeName,
+              };
+
+              if (positionIndex === -1) {
+                acc.push({
+                  position: winner.position,
+                  users: [newUser],
+                });
+              } else {
+                acc[positionIndex].users.push(newUser);
+              }
+            } else {
+              // Individual Winners
+              winner.eventRegistration.participants.user.forEach(
+                (participant) => {
+                  const positionIndex = acc.findIndex(
+                    (w) => w.position === winner.position,
+                  );
+                  const newUser = {
+                    name: participant.name,
+                    college: participant.college || "Unknown College",
+                    year: participant.year_of_study || "N/A",
+                  };
+
+                  if (positionIndex === -1) {
+                    acc.push({
+                      position: winner.position,
+                      users: [newUser],
+                    });
+                  } else {
+                    acc[positionIndex].users.push(newUser);
+                  }
+                },
+              );
+            }
+            return acc;
+          }, [])
+          .sort((a, b) => a.position - b.position),
+      }));
+      // console.log(formattedData)
+      setSelectedProgram(formattedData[0]);
+      setShowPoster(true);
+      setIsDialogOpen(true);
+      // console.log("Formatted Data:", formattedData[0]);
+    } catch (error) {
+      console.error("Failed to select program", error);
+    } finally {
+      setPosterLoading(false);
+    }
+  };
 
   const isNewRelease = (dateString) => {
     // console.log(dateString);
@@ -61,6 +165,74 @@ function Index() {
     return timeDifference <= 30;
   };
 
+  const handleOpenChange = () => {
+    setIsDialogOpen(!isDialogOpen);
+  };
+
+  const handleDownload = () => {
+    const poster = document.getElementById("resultPosterId");
+    ReactGA.event({
+      category: "Button",
+      action: "Click",
+      label: "Download result",
+    });
+    html2canvas(poster, {
+      scale: 5,
+      useCORS: true,
+    }).then((canvas) => {
+      const imageUrl = canvas.toDataURL("image/png");
+      setImageUrl(imageUrl);
+      const link = document.createElement("a");
+      link.href = imageUrl;
+      if (selectedProgram) {
+        link.download = `${selectedProgram.programName}-result.png`;
+      } else {
+        link.download = "poster.png";
+      }
+      link.click();
+    });
+  };
+
+  const handleShare = async () => {
+    const poster = document.getElementById("resultPosterId");
+    ReactGA.event({
+      category: "Button",
+      action: "Click",
+      label: "Share now result",
+    });
+    html2canvas(poster, {
+      scale: 5,
+      useCORS: true,
+    }).then((canvas) => {
+      canvas.toBlob(async (blob) => {
+        if (!blob) return;
+
+        const file = new File([blob], "poster.png", { type: "image/png" });
+
+        if (navigator.share) {
+          try {
+            await navigator.share({
+              title: "Kalama",
+              url: "https://czonekalama.in",
+              text: "Check out the winners! 🎉",
+              files: [file],
+            });
+          } catch (err) {
+            console.error("Error sharing:", err);
+          }
+        } else {
+          console.warn(
+            "Web Share API not supported or file sharing not supported",
+          );
+          alert(
+            "Sorry, file sharing is not supported on your device please download the image and share it manually",
+          );
+        }
+      });
+    });
+  };
+
+  const colors = ["#3592BA", "#00A99D", "#8DC63F", "#FF5733", "#FFC300"];
 
   function ExtractedText(text) {
     if (!text) {
@@ -85,36 +257,72 @@ function Index() {
   }
 
   return (
-    <div className='w-full p-4 z-10 relative'>
+    <div className="w-full p-4 z-10 relative">
       <Header title="Results" href="/" />
-      <section className='max-w-[700px] mx-auto' ref={parent}>
-        <div className='mt-10'>
+      <section className="max-w-[700px] mx-auto" ref={parent}>
+        <div className="mt-10">
           {/* Search Box */}
-          <div className="flex mb-4 items-center justify-center gap-[10px] w-full max-w-[360px] mx-auto">
-            <div 
-              className='rounded-[200px] relative h-[30px] w-[30px] flex items-center justify-center flex-shrink-0'
-              style={{
-                background: 'radial-gradient(50% 50% at 50% 50%, #0F5BA8 0%, #022564 100%)'
-              }}
-            >
-              <img src={SearchIcon} alt="Search" className="w-4 h-4" />
-            </div>
+          <div className="flex items-center justify-center w-full p-2 border border-gray-800 shadow-sm max-w-[400px] mx-auto focus-within:border-blue-500 focus-within:shadow-md">
+            <img src={SearchIcon} alt="Search Icon" className="w-6 h-6" />
             <input
               type="text"
               placeholder="Search programs..."
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              className="ring-0 focus:ring-0 focus:outline-none w-full rounded-[200px] border border-borderColor px-4 py-2 h-[30px]"
+              className="ring-0 focus:ring-0 focus:outline-none w-full pl-2"
             />
           </div>
-
         </div>
 
+        {filteredPrograms.length > 0 && selectedProgram && showPoster && (
+          <div className="min-h-[400px] pb-4">
+            {/* {loadingPoster ? (
+                  <div className='flex items-center justify-center py-4'>
+                    <Loader className='animate-spin' />
+                  </div>
+                ) : ( */}
+            <div className="flex items-center justify-center pt-6" ref={parent}>
+              {posterLoading ? (
+                <div className="w-full h-full min-h-[400px] max-w-[360px] bg-slate-200 animate-pulse"></div>
+              ) : (
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 20 }}
+                  transition={{ duration: 0.4 }}
+                >
+                  <div className="w-fit h-fit" id="resultPosterId">
+                    <Poster data={selectedProgram} />
+                  </div>
+                </motion.div>
+              )}
+            </div>
+            {/* )} */}
+
+            <div className="flex items-center justify-center gap-2 mt-4 max-w-[300px] mx-auto">
+              <button
+                onClick={handleShare}
+                className="flex flex-1 text-center justify-center items-center gap-1 border-2 border-borderColor px-2 py-1 hover:bg-black hover:text-white transition-all ease-in-out duration-300"
+              >
+                <Share2 className="w-4 h-4" />
+                <p className="font-semibold">Share</p>
+              </button>
+              <button
+                onClick={handleDownload}
+                className="flex flex-1 text-center justify-center items-center gap-1 border-2 border-borderColor px-2 py-1 hover:bg-black hover:text-white transition-all ease-in-out duration-300"
+              >
+                {" "}
+                <Download className="w-4 h-4" />
+                <p className="font-semibold">Download</p>
+              </button>
+            </div>
+          </div>
+        )}
         {/* Program List */}
         {/* { */}
-        <div className='mt-10 w-full mx-auto'>
+        <div className="mt-10 w-full mx-auto">
           <motion.div
-            className='flex flex-wrap gap-4 items-center justify-center w-full mx-auto'
+            className="flex flex-wrap gap-4 items-center justify-center w-full mx-auto"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 20 }}
@@ -128,7 +336,7 @@ function Index() {
                     <motion.button
                       key={index}
                       onClick={() => handleProgramSelect(program)}
-                      className='bg-[#605F5F] text-[#012161] border-[1.6px] bg-transparent cursor-pointer border-b-[4px] hover:border-b-[2px] border-borderColor px-4 py-1 rounded-xl shadow-md flex items-center justify-center leading-5 relative'
+                      className="bg-[#605F5F] text-[#012161] border-[1.6px] bg-transparent cursor-pointer border-b-[4px] hover:border-b-[2px] border-borderColor px-4 py-1 rounded-xl shadow-md flex items-center justify-center leading-5 relative"
                       initial={{ opacity: 0, y: 20 }}
                       animate={{ opacity: 1, y: 0 }}
                       exit={{ opacity: 0, y: 20 }}
@@ -137,55 +345,54 @@ function Index() {
                     >
                       {ExtractedText(program?.name)}
                       {isNewRelease(program?.last_updated) && (
-                        <span className='text-[10px] bg-red-500 text-white px-1 h-[16px] flex items-center justify-center py-[0.1px] rounded-md ml-2 absolute -top-2 -right-3'>
+                        <span className="text-[10px] bg-red-500 text-white px-1 h-[16px] flex items-center justify-center py-[0.1px] rounded-md ml-2 absolute -top-2 -right-3">
                           New
                         </span>
                       )}
                     </motion.button>
                   ))
+                ) : programs.length === 0 ? (
+                  <div>
+                    <p className="text-gray-500 flex flex-col text-center">
+                      Not Results Published Yet
+                    </p>
+                  </div>
                 ) : (
-                  programs.length === 0 ? (
-                    <div>
-                      <p className='text-gray-500 flex flex-col text-center'>
-                        Not Results Published Yet
-                      </p>
-                    </div>
-                  ) : (
-                    <motion.p
-                      className='text-gray-500 flex flex-col text-center'
-                      initial={{ opacity: 0, scale: 0.8 }}
-                      animate={{ opacity: 1, scale: 1 }}
-                      transition={{ duration: 0.5 }}
-                    >
-                      No Results found.
-                      <img src={Empty} alt="empty" className="mt-2 max-w-[380px]" />
-                    </motion.p>
-                  )
+                  <motion.p
+                    className="text-gray-500 flex flex-col text-center"
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                  >
+                    No Results found.
+                    <img
+                      src={Empty}
+                      alt="empty"
+                      className="mt-2 max-w-[380px]"
+                    />
+                  </motion.p>
                 )}
               </>
             ) : (
-              <div className='flex items-center justify-center w-full py-6'>
+              <div className="flex items-center justify-center w-full py-6">
                 {/* <Loader className='animate-spin' /> */}
                 <img src={Loading} alt="loading" className="max-w-[360px]" />
               </div>
             )}
           </motion.div>
         </div>
+
         {/* // ) : (
           //   <div className='flex items-center justify-center w-full py-6'>
           //     <img src={Loading} alt="loading" className="max-w-[360px]" />
           //   </div>
           // )
         // } */}
-      </section >
-
-
-
+      </section>
 
       {/* <Poster /> */}
-    </div >
+    </div>
   );
 }
 
 export default Index;
-
